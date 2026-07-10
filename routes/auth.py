ca5cc2d6b6bf9e9a380fa62293a8baa2809fb6db
Venkatesh Patte <90545009+ursvenkat@users.noreply.github.com>
2026-07-10 13:17:35 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -6,3 +6,18 @@ Features:
 - Login User
 - JWT Authentication
 """
+from flask import Blueprint, jsonify
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/register")
+def register():
+    return jsonify({
+        "message": "User Registration Endpoint"
+    })
+
+@auth_bp.route("/login")
+def login():
+    return jsonify({
+        "message": "User Login Endpoint"
+    })
