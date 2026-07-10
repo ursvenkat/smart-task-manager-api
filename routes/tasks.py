d0c6896925a5b52aa573baba2c8c981f7d06ee19
Venkatesh Patte <90545009+ursvenkat@users.noreply.github.com>
2026-07-10 13:17:49 +0530

    Update tasks.py

--- routes/tasks.py
+++ routes/tasks.py
@@ -7,3 +7,12 @@ Features:
 - Delete Task
 - View Tasks
 """
+from flask import Blueprint, jsonify
+
+task_bp = Blueprint("tasks", __name__)
+
+@task_bp.route("/tasks")
+def get_tasks():
+    return jsonify({
+        "message": "List of Tasks"
+    })
