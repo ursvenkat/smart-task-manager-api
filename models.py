a80e95d9b6a3d17c921017b351d4e247e84de2e0
Venkatesh Patte <90545009+ursvenkat@users.noreply.github.com>
2026-07-10 13:13:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
+from database import db
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    priority = db.Column(db.String(20))
+    status = db.Column(db.String(20), default="PENDING")
