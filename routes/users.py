789e2df45212546b4fecdd56518169b6a8757538
Venkatesh Patte <90545009+ursvenkat@users.noreply.github.com>
2026-07-10 13:18:05 +0530

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -6,3 +6,12 @@ Features:
 - Update User
 - Delete User
 """
+from flask import Blueprint, jsonify
+
+user_bp = Blueprint("users", __name__)
+
+@user_bp.route("/users")
+def get_users():
+    return jsonify({
+        "message": "List of Users"
+    })
